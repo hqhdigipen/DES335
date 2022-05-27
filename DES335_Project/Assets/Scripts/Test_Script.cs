eb0f8761f2baf87094139898d9df5ca3db109c39
weizhegoh <65543173+weizhegoh@users.noreply.github.com>
2022-05-27 21:12:18 +0800

    Testing
    Output Hello World!

--- DES335_Project/Assets/Scripts/Test_Script.cs
+++ DES335_Project/Assets/Scripts/Test_Script.cs
@@ -7,7 +7,7 @@ public class Test_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello!!");
     }
 
     // Update is called once per frame
