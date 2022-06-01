308ea775c51365e262862b9c055eb10fa87b921a
Qi hao <75103107+hqhdigipen@users.noreply.github.com>
2022-06-01 14:14:36 +0800

    Combat UI Complete

--- DES335_Project/Assets/Scripts/PlayerCombat/CombatMenuScript.cs
+++ DES335_Project/Assets/Scripts/PlayerCombat/CombatMenuScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class CombatMenuScript : MonoBehaviour
@@ -22,56 +23,78 @@ public class CombatMenuScript : MonoBehaviour
 
     private void Update()
     {
-        if (currState == "Main")
-        {
-            ItemMenu.SetActive(false);
-            AttackMenu.SetActive(false);
-            CombatMenu.SetActive(true);
-        }
+        //Debug.Log(currState);
 
+        switch (currState)
+        {
+            case "Main":
+                ItemMenu.SetActive(false);
+                AttackMenu.SetActive(false);
+                CombatMenu.SetActive(true);
+                pointer.SetActive(false);
+                break;
+
+            case "Attack":
+                AttackMenu.SetActive(true);
+                CombatMenu.SetActive(false);
+                pointer.SetActive(false);
+                break;
+
+            case "Item":
+                ItemMenu.SetActive(true);
+                CombatMenu.SetActive(false);
+                break;
+
+            case "Targeting":
+                AttackMenu.SetActive(false);
+                pointer.SetActive(true);
+                break;
+        }
 
         if (Input.GetMouseButtonDown(1))
         {
-            
+            if (currState == "Targeting")
+            {
+                currState = "Attack";
+            }
+            else
+            {
+                currState = "Main";
+            }
         }
 
-        if (currState == "Targeting" && Input.GetMouseButtonDown(0))
+        if (currState == "Targeting")
         {
-           
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null && hit.collider.transform.tag == "Enemy")
+            {
+                //Debug.Log("Target name: " + hit.collider.name);
+                pointer.transform.position = hit.collider.transform.position + new Vector3(0, (hit.collider.transform.GetComponent<SpriteRenderer>().sprite.bounds.size.y * 3.5f / 2) + (pointer.transform.GetComponent<SpriteRenderer>().sprite.bounds.size.y * 4.0f / 2), 0);
+            }
+
+            if (Input.GetMouseButtonDown(0) && hit.collider.transform.tag == "Enemy")
+            {
+                Debug.Log("Target name: " + hit.collider.name);
+                hit.collider.transform.GetComponent<CharScript>().TakeDamage(10);
+                currState = "Main";
+            }
         }
     }
 
+
     public void AttackButton()
     {
-        AttackMenu.SetActive(true);
-        CombatMenu.SetActive(false);
+        currState = "Attack";
     }
 
     public void ItemButton()
     {
-        ItemMenu.SetActive(true);
-        CombatMenu.SetActive(false);
-    }
-
-    public void DefendButton()
-    {
-
-    }
-
-    public void RunButton()
-    {
-
+        currState = "Item";
     }
 
     public void Attack1Button()
     {
-        currState = "Targeting";
-        AttackMenu.SetActive(false);
-        pointer.SetActive(true);
-    }
-
-    public void OnPointerClick(PointerEventData eventData) // 3
-    {
-        clickedEnemy.TakeDamage(10);
+        currState = "Targeting";      
     }
 }
