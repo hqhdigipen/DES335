99f0d8f8701b0aed4d47384e7fae18408fe0a0d2
weizhegoh <65543173+weizhegoh@users.noreply.github.com>
2022-06-01 15:23:45 +0800

    Added Enemy Attack Logic

--- DES335_Project/Assets/Scripts/Enemy/Enemy.cs
+++ DES335_Project/Assets/Scripts/Enemy/Enemy.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public string unitName;
-    public int unitLevel;
-    public int damage;
-    public int maxHP;
-    public int currentHP;
+    public GameObject enemy1;
+    public GameObject enemy2;
+
+    //var AttackList = new List<string> { "Attack1", "Attack2", "Attack3", "Attack4" };
+
+    //var random_attack = new Random();
+    //int i = random.Next(AttackList.Count);
+    //Debug.Log(AttackList[0]);
 }
