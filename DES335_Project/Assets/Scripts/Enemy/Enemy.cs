804711b1ff11a540e425e4fc54119fe757eae885
weizhegoh <65543173+weizhegoh@users.noreply.github.com>
2022-06-03 15:12:18 +0800

    Enemy random attack to damage player

--- DES335_Project/Assets/Scripts/Enemy/Enemy.cs
+++ DES335_Project/Assets/Scripts/Enemy/Enemy.cs
@@ -2,33 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Enemy : MonoBehaviour
 {
     public enum ENEMY_TYPE 
     { 
-      FIRE, 
-      WATER, 
-      EARTH 
+      Fire, 
+      Water, 
+      Earth 
     };
     
     public ENEMY_TYPE Element_Type;
+    public GameObject player;
 
     public List<string> attackList = new List<string>();
-    private void AttackStart()
+    public List<int> DamageList = new List<int>();
+    public void Enemy_Attack()
     {
-
-       // CheckElement();
-        //List<string> AttackList = new List<string> { "Attack1", "Attack2", "Attack3", "Attack4" };
         int i = Random.Range(0, attackList.Count);
-
         Debug.Log("Attack is" + attackList[i]);
     }
 
-    private void CheckElement() 
+    private void Update()
     {
-        //if(ENEMY_TYPE.FIRE)
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.GetComponent<CharScript>().TakeDamage(10, Element_Type.ToString());
+            Debug.Log("A key is pressed, player taking 10 damage");
 
+        }
     }
 }
