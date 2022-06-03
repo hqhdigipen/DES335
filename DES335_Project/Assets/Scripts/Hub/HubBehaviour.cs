e680c64b8e4cf482e7dd494f69610d416130b1b4
brinaa <brina_chia@hotmail.com>
2022-06-03 14:36:16 +0800

    Blacksmith Done

--- DES335_Project/Assets/Scripts/Hub/HubBehaviour.cs
+++ DES335_Project/Assets/Scripts/Hub/HubBehaviour.cs
@@ -5,23 +5,64 @@ using UnityEngine;
 
 public class HubBehaviour : MonoBehaviour
 {
-    public GameObject generalStoreCanvas, blacksmithCanvas, motherTreeCanvas;
+    public GameObject generalStoreCanvas, blacksmithCanvas, motherTreeCanvas, hubCanvas;
 
-    public void Update()
+    public GameObject playerInfo, allyInfo;
+
+    public void Start()
     {
         blacksmithCanvas.SetActive(false);
         generalStoreCanvas.SetActive(false);
         motherTreeCanvas.SetActive(false);
+        hubCanvas.SetActive(true);
+
+        playerInfo.SetActive(true);
+        allyInfo.SetActive(false);
+    }
+    public void Update()
+    {
+       
     }
 
     public void OpenBlacksmithCanvas()
     {
-            blacksmithCanvas.SetActive(true);
-        
+        playerInfo.SetActive(true);
+        blacksmithCanvas.SetActive(true);
+        hubCanvas.SetActive(false);
     }
 
+    public void OpenMotherTreeCanvas()
+    {
+        motherTreeCanvas.SetActive(true);
+        hubCanvas.SetActive(false);
+    }
 
+    public void OpenGeneralStoreCanvas()
+    {
+        generalStoreCanvas.SetActive(true);
+        hubCanvas.SetActive(false);
+    }
 
-  
+    public void CloseCanvas()
+    {
+        if (blacksmithCanvas.activeSelf) {
+            blacksmithCanvas.SetActive(false);
+            allyInfo.SetActive(false);
+            playerInfo.SetActive(false);
+            hubCanvas.SetActive(true);
+        }
+       
+    }
+
+    public void OpenPlayerDetails() {
+        playerInfo.SetActive(true);
+        allyInfo.SetActive(false);
+    }
+
+    public void OpenAllyDetails()
+    {
+        allyInfo.SetActive(true);
+        playerInfo.SetActive(false);
+    }
 }
 
