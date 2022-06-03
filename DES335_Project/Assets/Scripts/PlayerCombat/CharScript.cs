b9d19bad25411cbdffbbe475fa1726d44e2836c4
Qi hao <75103107+hqhdigipen@users.noreply.github.com>
2022-06-03 14:35:46 +0800

    Rename ManagerScript, Add TypeWeakness, Add TempAssets

--- DES335_Project/Assets/Scripts/PlayerCombat/CharScript.cs
+++ DES335_Project/Assets/Scripts/PlayerCombat/CharScript.cs
@@ -7,6 +7,11 @@ public class CharScript : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    float typeAdvantageMod = 1.25f;
+    float typeWeaknessMod = 0.75f;
+    public enum ElementType { Fire, Water, Earth }
+    public ElementType elementType;
+
     public HealthBarScript healthBar;
 
     // Start is called before the first frame update
@@ -21,13 +26,59 @@ public class CharScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            TakeDamage(20);
+            TakeDamage(20, "Fire");
         }
     }
 
-    public void TakeDamage(int damage)
+    public void TakeDamage(int damage, string atkElement)
     {
-        currentHealth -= damage;
+        switch (elementType.ToString())
+        {
+            case "Fire":
+                switch (atkElement)
+                {
+                    case "Fire":
+                        currentHealth -= damage;
+                        break;
+                    case "Water":
+                        currentHealth -= Mathf.FloorToInt(damage * typeAdvantageMod);
+                        break;
+                    case "Earth":
+                        currentHealth -= Mathf.FloorToInt(damage * typeWeaknessMod);
+                        break;
+                }
+                break;
+            case "Water":
+                switch (atkElement)
+                {
+                    case "Fire":
+                        currentHealth -= Mathf.FloorToInt(damage * typeWeaknessMod);
+                        break;
+                    case "Water":
+                        currentHealth -= damage;
+                        break;
+                    case "Earth":
+                        currentHealth -= Mathf.FloorToInt(damage * typeAdvantageMod);
+                        break;
+                }
+                break;
+            case "Earth":
+                switch (atkElement)
+                {
+                    case "Fire":
+                        currentHealth -= Mathf.FloorToInt(damage * typeAdvantageMod);
+                        break;
+                    case "Water":
+                        currentHealth -= Mathf.FloorToInt(damage * typeWeaknessMod);
+                        break;
+                    case "Earth":
+                        currentHealth -= damage;
+                        break;
+                }
+                break;
+
+        }
+
         healthBar.SetHealth(currentHealth);
     }
 }
