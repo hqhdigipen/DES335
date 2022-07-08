7af4e5e376c34a5f6664f8c22b5530353301cc4f
weizhegoh <65543173+weizhegoh@users.noreply.github.com>
2022-07-08 14:00:42 +0800

    added back to hub button

--- DES335_Project/Assets/Scripts/Game States/GameState.cs
+++ DES335_Project/Assets/Scripts/Game States/GameState.cs
@@ -13,15 +13,100 @@ public class GameState : MonoBehaviour
         LOSE,
     }
 
-    // Start is called before the first frame update
+    int g_state = 0; 
+
+    GameObject companion, enemy, enemy2, player;
+    public GameObject VictoryButton, BackButton;
+   
+// Start is called before the first frame update
     void Start()
     {
-        
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemy2 = GameObject.FindGameObjectWithTag("Enemy2");
+        player = GameObject.FindGameObjectWithTag("Player");
+        companion = GameObject.FindGameObjectWithTag("Companion");
+
+        if (VictoryButton != null)
+        {
+            VictoryButton.SetActive(false);
+        }
+
+        if (BackButton != null)
+        {
+            BackButton.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemy != null && enemy2 != null)
+        {
+            if (enemy.GetComponent<CharScript>().currentHealth <= 0 && enemy2.GetComponent<CharScript>().currentHealth <= 0)
+            {
+                g_state = (int)GameState.state.WIN;
+                Application.LoadLevel("WinLose");
+
+            }
+        }
+
+        if (player != null && companion != null)
+        {
+            if (player.GetComponent<CharScript>().currentHealth <= 0 && companion.GetComponent<CharScript>().currentHealth <= 0)
+            {
+                g_state = (int)GameState.state.LOSE;
+            }
+        }
+
+        if(g_state == (int)GameState.state.MAIN_MENU)
+        {
+
+        }
+        else if (g_state == (int)GameState.state.HUB)
+        {
+
+        }
+        else if (g_state == (int)GameState.state.COMBAT)
+        {
+
+        }
+        else if (g_state == (int)GameState.state.WIN)
+        {
+            ContinuePopUp();
+        }
+        else if (g_state == (int)GameState.state.LOSE)
+        {
+
+        }
+    }
+
+    void ContinuePopUp()
+    {
+        if (VictoryButton != null)
+        {
+            VictoryButton.SetActive(true);
+        }
+    }
+
+    public void ContinueButtonPressed()
+    {
+        Debug.Log("Continue Button Pressed");
+    }
+
+    void BackPopUp()
+    {
+        if (BackButton != null)
+        {
+            BackButton.SetActive(true);
+        }
+    }
+
+    public void BackButtonPressed()
+    {
+        Debug.Log("Back Button Pressed");
+        g_state = (int)GameState.state.HUB;
+
+        Application.LoadLevel("Hub");
+
     }
 }
