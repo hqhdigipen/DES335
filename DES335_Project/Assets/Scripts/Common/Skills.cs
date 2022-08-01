86e839d55130075ce50a1e7d5d9850e88c958529
weizhegoh <65543173+weizhegoh@users.noreply.github.com>
2022-08-02 03:31:20 +0800

    Removed Defend button and balanced skills

--- DES335_Project/Assets/Scripts/Common/Skills.cs
+++ DES335_Project/Assets/Scripts/Common/Skills.cs
@@ -53,46 +53,46 @@ public class Skills : MonoBehaviour
     public GameObject player, companion;
 
     //Player
-    Skill PlayerEqLvl2Skill1 = new Skill("Slam", 30, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerEqLvl2Skill2 = new Skill("Flame Rush", 50, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl2Skill3 = new Skill("Blaze Kick", 50, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl2Skill4 = new Skill("Inferno Claw ", 50, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl2Skill1 = new Skill("Slam", 15, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl2Skill2 = new Skill("Flame Rush", 20, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl2Skill3 = new Skill("Blaze Kick", 25, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl2Skill4 = new Skill("Inferno Claw ", 30, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
 
-    Skill PlayerEqLvl3Skill1 = new Skill("Hyper Punch", 40, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerEqLvl3Skill2 = new Skill("Flame Blitz", 60, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl3Skill3 = new Skill("Blaze Gigs", 60, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl3Skill4 = new Skill("Inferno Blast", 60, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl3Skill1 = new Skill("Hyper Punch", 20, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl3Skill2 = new Skill("Flame Blitz", 25, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl3Skill3 = new Skill("Blaze Gigs", 30, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl3Skill4 = new Skill("Inferno Blast", 35, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
 
-    Skill PlayerEqLvl4Skill1 = new Skill("Sonic Boom", 50, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerEqLvl4Skill2 = new Skill("Flame Nado", 70, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl4Skill3 = new Skill("Blaze Tempest", 70, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl4Skill4 = new Skill("Inferno Impact", 70, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl4Skill1 = new Skill("Sonic Boom", 25, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl4Skill2 = new Skill("Flame Nado", 30, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl4Skill3 = new Skill("Blaze Tempest", 35, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl4Skill4 = new Skill("Inferno Impact", 40, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
 
-    Skill PlayerEqLvl5Skill1 = new Skill("Sonic Punch", 60, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerEqLvl5Skill2 = new Skill("Flame Call", 70, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl5Skill3 = new Skill("Blade Inferno", 70, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill PlayerAmourLvl5Skill4 = new Skill("Inferno Infinite", 70, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl5Skill1 = new Skill("Sonic Punch", 30, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerEqLvl5Skill2 = new Skill("Flame Call", 35, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl5Skill3 = new Skill("Blade Inferno", 40, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill PlayerAmourLvl5Skill4 = new Skill("Inferno Infinite", 45, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
 
     //Companion
-    Skill AllyEqLvl2Skill1 = new Skill("Skill1Level2", 50, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyEqLvl2Skill2 = new Skill("Skill2Level2", 50, 5, SKILL_TYPE.Earth, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl2Skill3 = new Skill("Skill3Level2", 50, 5, SKILL_TYPE.Earth, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl2Skill4 = new Skill("Skill4Level2", 50, 5, SKILL_TYPE.Earth, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl2Skill1 = new Skill("Magic Ring", 10, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl2Skill2 = new Skill("Water Jet", 15, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl2Skill3 = new Skill("Aqua Splash", 20, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl2Skill4 = new Skill("Hydro Blast", 25, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
 
-    Skill AllyEqLvl3Skill1 = new Skill("Skill1Level3", 60, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyEqLvl3Skill2 = new Skill("Skill2Level3", 60, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl3Skill3 = new Skill("Skill3Level3", 60, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl3Skill4 = new Skill("Skill4Level3", 60, 5, SKILL_TYPE.Fire, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl3Skill1 = new Skill("Magic Whirwind", 15, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl3Skill2 = new Skill("Water Rush", 20, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl3Skill3 = new Skill("Aqua Waterfall", 25, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl3Skill4 = new Skill("Hydro Pump", 30, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
 
-    Skill AllyEqLvl4Skill1 = new Skill("Skill1Level4", 70, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyEqLvl4Skill2 = new Skill("Skill2Level4", 70, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl4Skill3 = new Skill("Skill3Level4", 70, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl4Skill4 = new Skill("Skill4Level4", 70, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl4Skill1 = new Skill("Magic Vortex", 20, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl4Skill2 = new Skill("Water Blade", 25, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl4Skill3 = new Skill("Aqua Surf", 30, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl4Skill4 = new Skill("Hydro Beam", 35, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
 
-    Skill AllyEqLvl5Skill1 = new Skill("Skill1Level5", 70, 5, SKILL_TYPE.Normal, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyEqLvl5Skill2 = new Skill("Skill2Level5", 70, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl5Skill3 = new Skill("Skill3Level5", 70, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
-    Skill AllyAmourLvl5Skill4 = new Skill("Skill4Level5", 70, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl5Skill1 = new Skill("Magic Tornado", 25, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyEqLvl5Skill2 = new Skill("Water Bolt", 30, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl5Skill3 = new Skill("Aqua Tsunami", 35, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
+    Skill AllyAmourLvl5Skill4 = new Skill("Hydro Blast", 40, 5, SKILL_TYPE.Water, DAMAGE_TYPE.SINGLE_TARGET);
 
     private void Start()
     {
