2e47b74fcfed975765529c44493df05c2a372195
Qi hao <hqhdigipen@github.com>
2022-08-04 20:45:54 +0800

    Reflect current skill on Hub UI Done

--- DES335_Project/Assets/Scripts/Hub/UIScript.cs
+++ DES335_Project/Assets/Scripts/Hub/UIScript.cs
@@ -9,7 +9,7 @@ public class UIScript : MonoBehaviour
     public Skills skill_Player, skill_Comp;
 
     [SerializeField]
-    public List<TextMeshProUGUI> playerSkill1, playerSkill2, playerSkill3, playerSkill4, CompSkill1, CompSkill2, CompSkill3, CompSkill4;
+    public List<TextMeshProUGUI> playerSkill1, playerSkill2, playerSkill3, playerSkill4, compSkill1, compSkill2, compSkill3, compSkill4;
 
     // Start is called before the first frame update
     void Awake()
@@ -37,17 +37,38 @@ public class UIScript : MonoBehaviour
 
             playerSkill4.Add(fooObj.GetComponentInChildren<TextMeshProUGUI>());
         }
+
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("CompSkill_1"))
+        {
+
+            compSkill1.Add(fooObj.GetComponentInChildren<TextMeshProUGUI>());
+        }
+
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("CompSkill_2"))
+        {
+
+            compSkill2.Add(fooObj.GetComponentInChildren<TextMeshProUGUI>());
+        }
+
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("CompSkill_3"))
+        {
+
+            compSkill3.Add(fooObj.GetComponentInChildren<TextMeshProUGUI>());
+        }
+
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("CompSkill_4"))
+        {
+
+            compSkill4.Add(fooObj.GetComponentInChildren<TextMeshProUGUI>());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerSkill1.Count != 0)
+        foreach (TextMeshProUGUI fooObj in playerSkill1)
         {
-            foreach (TextMeshProUGUI fooObj in playerSkill1)
-            {
-                fooObj.text = skill_Player.Skill_List[0].Name;
-            }
+            fooObj.text = skill_Player.Skill_List[0].Name;
         }
 
         foreach (TextMeshProUGUI fooObj in playerSkill2)
@@ -63,6 +84,26 @@ public class UIScript : MonoBehaviour
         foreach (TextMeshProUGUI fooObj in playerSkill4)
         {
             fooObj.text = skill_Player.Skill_List[3].Name;
+        }
+
+        foreach (TextMeshProUGUI fooObj in compSkill1)
+        {
+            fooObj.text = skill_Comp.Skill_List[0].Name;
+        }
+
+        foreach (TextMeshProUGUI fooObj in compSkill2)
+        {
+            fooObj.text = skill_Comp.Skill_List[1].Name;
+        }
+
+        foreach (TextMeshProUGUI fooObj in compSkill3)
+        {
+            fooObj.text = skill_Comp.Skill_List[2].Name;
+        }
+
+        foreach (TextMeshProUGUI fooObj in compSkill4)
+        {
+            fooObj.text = skill_Comp.Skill_List[3].Name;
         }
     }
 }
